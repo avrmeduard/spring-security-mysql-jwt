3ba1a46bd60b8c625fc6ef202c7d9f8bd8ddfaa5
avrm <avrmeduard@gmail.com>
2021-02-05 12:37:12 +0200

    dependencies injection added

--- src/main/java/com/avrm/springsecurity/controller/AuthController.java
+++ src/main/java/com/avrm/springsecurity/controller/AuthController.java
@@ -1,5 +1,11 @@
 package com.avrm.springsecurity.controller;
 
+import com.avrm.springsecurity.repository.RoleRepository;
+import com.avrm.springsecurity.security.jwt.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 }
 
