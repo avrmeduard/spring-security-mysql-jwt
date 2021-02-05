58f7f5b2375b862d98266804cc21688e0ecbd398
avrm <avrmeduard@gmail.com>
2021-02-05 12:27:38 +0200

    database relation updated. manyToMany

--- src/main/java/com/avrm/springsecurity/model/User.java
+++ src/main/java/com/avrm/springsecurity/model/User.java
@@ -33,7 +33,7 @@ public class User {
     @Size(max = 120)
     private String password;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "user_roles",
                joinColumns = @JoinColumn(name = "user_id"),
                inverseJoinColumns = @JoinColumn(name = "role_id"))
