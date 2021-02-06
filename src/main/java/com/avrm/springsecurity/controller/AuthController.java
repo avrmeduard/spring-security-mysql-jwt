ef5a6e9d34a74f59bf0c8d8f2e629f5c88338604
avrm <avrmeduard@gmail.com>
2021-02-06 14:54:14 +0200

    signin method added

--- src/main/java/com/avrm/springsecurity/controller/AuthController.java
+++ src/main/java/com/avrm/springsecurity/controller/AuthController.java
@@ -2,16 +2,24 @@ package com.avrm.springsecurity.controller;
 
 import com.avrm.springsecurity.dto.request.LoginRequest;
 import com.avrm.springsecurity.dto.request.SignupRequest;
+import com.avrm.springsecurity.dto.response.JwtResponse;
 import com.avrm.springsecurity.repository.RoleRepository;
 import com.avrm.springsecurity.security.jwt.JwtUtil;
+import com.avrm.springsecurity.security.service.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -32,8 +40,24 @@ public class AuthController {
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
-        return null;
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtUtil.generateToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                                                         .map(item -> item.getAuthority())
+                                                         .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getId(),
+                                                      userDetails.getUsername(),
+                                                      userDetails.getPassword(),
+                                                      roles));
+
     }
 
     @PostMapping("/signup")
